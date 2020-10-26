acc39a7434b9c116f0610a5d265f6b2ae5d465e7
saxakiil <vovakurguzov1999@yandex.ru>
2020-10-26 03:27:46 +0300

    Removing tokens and improving the code

--- cat_api.rb
+++ cat_api.rb
@@ -4,23 +4,20 @@ require 'json'
 
 class CatApi
 
-  TOKEN = 'c4826c41-cc56-4d51-b900-d928f7ba404f'
-  URL = "https://api.thecatapi.com/v1/images/search"
-
   public
   def get_url_image()
     content = get_content()
     json = JSON.parse(content)
-    return json[0]["url"]
+    return json[0]['url']
   end
 
   private
   def get_content()
 
-    uri = URI.parse(URL)
+    uri = URI.parse(ENV['URL_API'])
     http = Net::HTTP.new(uri.host)
     request = Net::HTTP::Get.new(uri.request_uri)
-    request.add_field('x-api-key', TOKEN)
+    request.add_field('x-api-key', ENV['TOKEN_API'])
     resp = http.request(request)
     return resp.body.to_s
   end
--- main.rb
+++ main.rb
@@ -2,14 +2,12 @@ require 'telegram/bot'
 require 'telegram/bot/types'
 require_relative 'cat_api'
 
-token = '1329024204:AAHEqtG16n0k66qw1UgIHvIuZigqHq4u1j0'
-
 def get_new_cat()
   cat = CatApi.new()
   return cat.get_url_image.to_s
 end
 
-Telegram::Bot::Client.run(token) do |bot|
+Telegram::Bot::Client.run(ENV['TOKEN_BOT']) do |bot|
 
   bot.listen do |rqst|
     Thread.start(rqst) do |request|
@@ -19,10 +17,10 @@ Telegram::Bot::Client.run(token) do |bot|
               Telegram::Bot::Types::KeyboardButton.new(text: 'Получить нового котика 🐈')
           ]
           markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, resize_keyboard: TRUE)
-          bot.api.send_message(chat_id: request.chat.id, text: "Привет! Меня зовут CatImg_bot и я могу отправлять тебе картиночки няшных котиков <3! \n Например вот:",
+          bot.api.send_message(chat_id: request.chat.id, text: 'Привет! Меня зовут CatImg_bot и я могу отправлять тебе картиночки няшных котиков <3! \n Например вот:',
                                )
           bot.api.send_photo(chat_id: request.chat.id, photo: get_new_cat)
-          bot.api.send_message(chat_id: request.chat.id, text: "Если захочешь ещё картинок, просто нажми на кнопку 'Получить нового котика 🐈'!",
+          bot.api.send_message(chat_id: request.chat.id, text: 'Если захочешь ещё картинок, просто нажми на кнопку \'Получить нового котика 🐈\'!',
                                reply_markup: markup)
         when 'Получить нового котика 🐈'
           bot.api.send_photo(chat_id: request.chat.id, photo: get_new_cat)
